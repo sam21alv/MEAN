5e2093fdbc0213573629d405d04d4b654a98bb4b
chenaoh <samantha.alvear18alv@gmail.com>
2021-09-27 13:17:28 -0500

    ejemplo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearProductoComponent } from './componts/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './componts/listar-productos/listar-productos.component';
 
-const routes: Routes = [];
+//componentes
+const routes: Routes = [
+  {path: '', component: ListarProductosComponent  },
+  {path: 'crear-producto', component: CrearProductoComponent},
+  {path: 'editar-producto/:id', component: CrearProductoComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
